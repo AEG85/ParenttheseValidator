3aaba5ead35232d57f9c8651632f09bf08dafbec
Evgeniy Akhtyrskikh <ahtirsk85@gmail.com>
2021-05-24 09:52:33 +0600

    Initial commit

---
+++ lib/Helpers/ParenthesesValidator.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace AEG85\Helpers;
+
+
+class ParenthesesValidator
+{
+    /**
+     * This method will help you check a string containing opening and closing parentheses.
+     * @param $string string
+     * @return bool
+     */
+    public static function validateBrackets(string $string): bool
+    {
+        if (empty($string) || !is_string($string))
+            return false;
+        //RegExp pattern
+        $exp = '/\A[^\(\)]*(\((?>[^\(\)]|(?1))*\)[^\(\)]*)+\z/';
+        preg_match($exp, $string, $matches);
+        if (empty($matches))
+            return false;
+        else
+            return true;
+    }
+}
